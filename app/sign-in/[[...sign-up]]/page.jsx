9039c221a1945418b54341651bcd70ce96e42db4
Rakshit Poojari <Rakshitpo54@gmail.com>
2023-08-23 09:45:40 +0530

    sign

--- app/sign-in/[[...sign-up]]/page.jsx
+++ app/sign-in/[[...sign-up]]/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { SignIn } from '@clerk/nextjs'
 const SignInPage = () => {
   return (
-    <div>SignIpPage</div>
-  )
+<>
+<SignIn/>
+</>
+    )
 }
 
 export default SignInPage
