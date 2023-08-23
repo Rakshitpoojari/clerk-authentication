9c92b639f2522650480dbb1762c831172411ad71
Rakshit Poojari <Rakshitpo54@gmail.com>
2023-08-23 09:55:57 +0530

    signout or logout

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,11 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-2xl font-bold.mb-5">Welcome to the Dashboard</h1>
+      <div className="p mb-5">
+        This is the demo site just to learn the nextjs & clerk tutorials.
+        go ahead ans use it just SignIn and SignUp
+      </div>
     </>
   )
 }
